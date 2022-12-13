3b93dca9b3acb1e44baf0ed568d2ed2bde2cb929
Nappa <kikbox3112@gmail.com>
2022-12-13 17:00:32 +0300

    +

--- Wow_maven_suddenly/src/test/java/org/example/ConfProperties.java
+++ Wow_maven_suddenly/src/test/java/org/example/ConfProperties.java
@@ -1,28 +1,33 @@
 package org.example;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfProperties {
+
     protected static FileInputStream fileInputStream;
     protected static Properties PROPERTIES;
+
     static {
         try {
-            //указание пути до файла с настройками
-            fileInputStream = new FileInputStream("src\\test\\java\\resources\\conf.properties");
+            fileInputStream = new FileInputStream("C:\\Users\\Kaban\\IdeaProjects\\Wow_maven_suddenly\\src\\test\\java\\resources");
             PROPERTIES = new Properties();
             PROPERTIES.load(fileInputStream);
         } catch (IOException e) {
             e.printStackTrace();
-            //обработка возможного исключения (нет файла и т.п.)
         } finally {
             if (fileInputStream != null)
                 try {
                     fileInputStream.close();
                 } catch (IOException e) {
-                    e.printStackTrace(); } } }
-    /**
-     * метод для возврата строки со значением из файла с настройками
-     */
+                    e.printStackTrace();
+                }
+        }
+    }
+
     public static String getProperty(String key) {
-        return PROPERTIES.getProperty(key); } }
+        return PROPERTIES.getProperty(key);
+    }
+}
--- Wow_maven_suddenly/src/test/java/org/example/YandexWebElements.java
+++ Wow_maven_suddenly/src/test/java/org/example/YandexWebElements.java
@@ -1,5 +1,57 @@
 package org.example;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class YandexWebElements {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
 
+    public WebDriver driver;
+
+    public YandexWebElements(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    //Yandex_ru Locator
+    @FindBy(xpath = "//span[@class='zen-ui-button-text__text']")
+    private WebElement enterInYandexAuthFieldsButton;
+
+    @FindBy(xpath = "//a[@class = 'CurrentAccount']")
+    private WebElement selectorYandexAddNewAccount;
+
+    @FindBy(xpath = "//span[@class = 'AddAccountButton-text']")
+    private WebElement newYandexAccountButton;
+
+    @FindBy(xpath = "//input[@id = 'passp-field-login']")
+    private WebElement loginYandexFieldEnter;
+
+    @FindBy(xpath = "//div[contains(@class, 'sign-in-button')]")
+    private WebElement signInYandexButton;
+
+    //Mail_ru Locator
+    @FindBy(xpath = "//button[contains(@class, 'resplash-btn')]")
+    private WebElement mailRuAuthButton;
+
+    @FindBy(xpath = "//input[contains(@name, 'username')]")
+    private WebElement authEnterMailFields;
+
+    @FindBy(xpath = "//button[contains(@data-test-id, 'next-button')]")
+    private WebElement enterPswrdButton;
+
+    @FindBy(xpath = "//input[@name= 'password']")
+    private WebElement passerdEnterFields;
+
+    @FindBy(xpath = "//span[text()= 'Sign in']")
+    private WebElement signInMailRuButton;
+
+    @FindBy(xpath = "//span[contains(text(), 'break-dance')]")
+    private WebElement neededMassageWithCodeFromMailBox;
+
+    @FindBy(xpath = "//td[@style= 'padding: 0 30px 30px;']/descendant::b[2]")
+    private WebElement getCodeFromMailRuMassage;
 }
